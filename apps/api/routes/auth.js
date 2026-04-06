fc453e88875d0b7f10d9e1e3567d31709107383c
kushalkumar-creator <kushalkumar5859@gmail.com>
2026-04-06 22:32:11 +0530

    fix auth callback

--- apps/api/routes/auth.js
+++ apps/api/routes/auth.js
@@ -9,7 +9,7 @@ const router = express.Router()
 passport.use(new GitHubStrategy({
   clientID: process.env.GITHUB_CLIENT_ID,
   clientSecret: process.env.GITHUB_CLIENT_SECRET,
-  callbackURL: '/api/auth/github/callback',
+  callbackURL: process.env.GITHUB_CALLBACK_URL,
   scope: ['user:email']
 }, async (accessToken, refreshToken, profile, done) => {
     try {
